488331c3158e77871852f7f80eee1186a3b89cc1
Jonathan Sutton <jonathans@al.insper.edu.br>
2021-09-30 12:54:03 -0300

    reuploadando arquivos

---
+++ notes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        string = (f'{self.id}.{self.title}')
+        return string
+
